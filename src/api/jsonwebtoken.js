c79da3c6e2d6f91eb2524f35a9051fa912774bf8
Ahmad Azri <azrirazak01@gmail.com>
2021-08-02 23:05:58 +0800

    add logout and cache implement basic cache

--- src/api/jsonwebtoken.js
+++ src/api/jsonwebtoken.js
@@ -3,15 +3,23 @@ const router = express.Router();
 
 const jwt = require('../middleware/jwt');
 
-
 router.post('/signin' , (req, res, next) => {
     res.json(jwt.signIn({email:req.body.email}));
 });
 
 router.get('/verify', jwt.verifyToken , (req, res, next) => {
   res.json({
-    message : 'token verified'
+      accesstoken: req.headers['newaccesstoken'] ? req.headers['newaccesstoken'] :  req.headers['accesstoken'],
+      publictoken: req.headers['publictoken'],
+      refreshtoken : req.headers['newaccesstoken'] ? true : false
   });
 });
+
+router.get('/signout', jwt.signOut , (req, res, next) => {
+  res.json({
+      message : 'sign out bye'
+  });
+});
+
 
 module.exports = router;
--- src/middleware/jwt/index.js
+++ src/middleware/jwt/index.js
@@ -1,29 +1,55 @@
 const jwt = require('jsonwebtoken');
-const { JWT_PRIVATE_KEY } = process.env 
+
+const NodeCache = require( "node-cache" );
+const nodecache = new NodeCache();
+
+const { JWT_PRIVATE_KEY, JWT_PUBLIC_KEY } = process.env 
 
 const generateToken = (payload, expiresIn) => {
     return expiresIn ? 
         jwt.sign(payload, JWT_PRIVATE_KEY, { expiresIn }) :
-        jwt.sign(payload, JWT_PRIVATE_KEY) ;
+        jwt.sign(payload, JWT_PUBLIC_KEY) ;
 }
 
 const signIn = (payload) => {
     const accessToken   = generateToken(payload, '1m');
     const publicToken   = generateToken(payload);
+    nodecache.set(publicToken, accessToken, 60 * 2);
     return {accessToken, publicToken};
 }
 
 const verifyToken = (req, res, next) => {
+    if(!nodecache.get(req.headers['publictoken'])){
+        next({
+            message : 'token not valid'
+        });
+        return;
+    }
+    
+    jwt.verify(req.headers['accesstoken'], JWT_PRIVATE_KEY, (err, decode) => {
+        if(err) req.headers['newaccesstoken'] = refreshToken(req.headers['publictoken'])
+    });
+
+    next();
+}
+
+const refreshToken = (publicToken) => {
     try {
-        jwt.verify(req.headers['accesstoken'], JWT_PRIVATE_KEY);
-        jwt.verify(req.headers['publictoken'], JWT_PRIVATE_KEY);
-        next();
+        const { email } = jwt.verify(publicToken, JWT_PUBLIC_KEY);
+        return generateToken( { email } , '1m');
     } catch (error) {
         next(error);
     }
 }
 
+const signOut = (req, res, next) => {
+    nodecache.del(req.headers['publictoken']);
+    next();
+}
+
 module.exports = {
     signIn,
-    verifyToken
+    verifyToken,
+    refreshToken,
+    signOut
 }
